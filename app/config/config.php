e6ebcf6c025ca1950d56c8105280b5f13e944ba8
Mushlih Almubarak <mushlih_31@mushlih.cf>
2022-08-10 18:23:27 +0300

    Change db info

--- app/config/config.php
+++ app/config/config.php
@@ -4,6 +4,6 @@ define('BASEURL', 'http://localhost:70/Belajar%20PHP%20MVC/public/');
 
 // Database
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_USER', 'mushlih_31');
+define('DB_PASS', 'Rahasia123');
 define('DB_NAME', 'php_mvc');
